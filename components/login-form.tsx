977089668dd1b07c9a3bb7ed972f9dccdfdba80b
Maxime Ginet <maxime.ginet@hotmail.com>
2026-01-12 12:23:04 +0100

    add all files update

---
+++ components/login-form.tsx
@@ -0,0 +1,74 @@
+"use client"
+
+import { GalleryVerticalEnd } from "lucide-react"
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <form className="flex flex-col gap-6">
+        <FieldGroup>
+          <div className="flex flex-col items-center gap-2 text-center">
+            <a
+              href="#"
+              className="flex flex-col items-center gap-2 font-medium"
+            >
+              <div className="flex size-8 items-center justify-center rounded-md bg-primary text-primary-foreground">
+                <GalleryVerticalEnd className="size-6" />
+              </div>
+              <span className="sr-only">Acme Inc.</span>
+            </a>
+            <h1 className="text-xl font-bold">Welcome to Acme Inc.</h1>
+            <FieldDescription>
+              Don&apos;t have an account?{" "}
+              <a href="/signup" className="underline hover:no-underline">
+                Sign up
+              </a>
+            </FieldDescription>
+          </div>
+
+          <Field>
+            <FieldLabel htmlFor="email">Email</FieldLabel>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </Field>
+
+          <Field>
+            <FieldLabel htmlFor="password">Password</FieldLabel>
+            <Input
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+              required
+            />
+          </Field>
+
+          <Button
+            type="button"
+            className="w-full"
+            onClick={() => {
+              window.location.href = "/otp"
+            }}
+          >
+            Login
+          </Button>
+        </FieldGroup>
+      </form>
+    </div>
+  )
+}
