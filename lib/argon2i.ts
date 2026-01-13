d16166e808cd19c8809ff36c2520fd3c90da7416
Maxime Ginet <maxime.ginet@hotmail.com>
2026-01-13 11:37:35 +0100

    add files argon2i

---
+++ lib/argon2i.ts
@@ -0,0 +1,24 @@
+import  argon2i  from "argon2"
+
+export const ArgonHash = async (hash: string) => {
+
+    try {
+        await argon2i.hash(hash)
+    } catch(e) {
+        console.log(e)
+        return false;
+    }
+
+}
+
+export const ArgonVerify = async (hash: string, verify: string ) => {
+
+    try {
+        await argon2i.verify(hash, verify)
+    } catch(e) {
+        console.log(e)
+        return false;
+    }
+
+
+}
